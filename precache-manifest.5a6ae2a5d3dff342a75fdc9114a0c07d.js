c84a39c1ee8135088076b3f85e48326b55e75929
Gilli Carmon <gillicarmon@gmail.com>
2019-03-23 15:13:20 +0200

    Updates

---
+++ precache-manifest.5a6ae2a5d3dff342a75fdc9114a0c07d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "01f8bf0756ee432ad8914d8c83819d00",
+    "url": "/PracticeWebsite/static/media/loving-you.01f8bf07.mp3"
+  },
+  {
+    "revision": "0d8d2b4dbb82b2c60284",
+    "url": "/PracticeWebsite/static/js/runtime~main.0d8d2b4d.js"
+  },
+  {
+    "revision": "4209352a2588b4f87076",
+    "url": "/PracticeWebsite/static/js/main.4209352a.chunk.js"
+  },
+  {
+    "revision": "78983e3aec276bba2afa",
+    "url": "/PracticeWebsite/static/js/1.78983e3a.chunk.js"
+  },
+  {
+    "revision": "4209352a2588b4f87076",
+    "url": "/PracticeWebsite/static/css/main.0dc7c78d.chunk.css"
+  },
+  {
+    "revision": "78983e3aec276bba2afa",
+    "url": "/PracticeWebsite/static/css/1.1215e824.chunk.css"
+  },
+  {
+    "revision": "76820d1a00643e7044b6a2ebd8891b0a",
+    "url": "/PracticeWebsite/index.html"
+  }
+];
